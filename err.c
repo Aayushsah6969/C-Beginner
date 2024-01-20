ec532edf16c3062d1e93ea756d7fd1b2c62551a7
aayushsah6969 <aayushsah269@gmail.com>
2024-01-20 13:10:41 +0530

    DOne

---
+++ err.c
@@ -0,0 +1,9 @@
+#include<math.h>
+#include<stdio.h>
+int main(){
+    float x;
+    x = 2.5;
+    float y = exp(x);
+    printf("%d %d",x,y);
+    return 0;
+}
---
+++ mathss.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+#define PI 3.1416
+#define MAX 180
+int main(){
+    int angle;
+    float x,y;
+    angle =0;
+    printf("            Angle            Cos(angle)\n\n");
+    while(angle<=MAX){
+        x=(PI/MAX)*angle;
+        y = cos(x);
+        printf("%15d %13.4f\n",angle,y);
+        angle = angle+10;
+    }
+    return 0;
+}
